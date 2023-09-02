192414a20c80a876d5b0195af86470b5f2c41888
Miguel <45602999+mespases@users.noreply.github.com>
2023-09-02 17:20:01 +0200

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import FilmCard from '../components/FilmCard.vue';
+  import jsonData from '@/assets/json/films.json'
+
+  const items = [{'title': 1}, {'title': 1}, {'title': 1}, {'title': 1}, {'title': 1}, {'title': 1}, {'title': 1}, {'title': 1}, {'title': 1}, {'title': 1}]
+</script>
+
+<template>
+  <section class="all_movies">
+    <FilmCard v-for="item in jsonData" :key="item.id" :item="item"/>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
